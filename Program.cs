872c41ac8f895a8e7a334af7e9dbce8254ee1d8a
JESUS ANDRES RIVERA ARCE <14170985@itculiacan.edu.mx>
2019-05-28 17:17:15 -0600

    Listo, esta bien feo pero hace lo que la profe dejo de tarea

--- Program.cs
+++ Program.cs
@@ -6,7 +6,16 @@ namespace Calcular_el_Error
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double suma1 = 1/(1+Math.Exp((-1)*(.7921 + .1921)));
+            double suma2 = 1/(1+Math.Exp((-1)*(.3887 + .8887)));
+            double suma3 = 1/(1+Math.Exp((-1)*(.2741 + .4741)));
+
+            double suma4 = 1/(1+Math.Exp( (-1) * ((suma1 * .202) + (suma2 * .394) + (suma3 * .806) )));
+
+            Console.WriteLine("Neurona 1: " + suma1);
+            Console.WriteLine("Neurona 2: " + suma2);
+            Console.WriteLine("Neurona 3: " + suma3);
+            Console.WriteLine("Salida: " + suma4);
         }
     }
 }
